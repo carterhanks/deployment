6cd6d2922138937710d77460c2099ca2994d7c94
carterhanks <carterhanks@gmail.com>
2021-06-24 15:08:20 -0600

    Image and Name Added

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile( path.join(__dirname, '../index.html') );
+    res.sendFile( path.join(__dirname, '../index.html', '../index.css') );
 });
 
 const port = process.env.PORT || 5050;
